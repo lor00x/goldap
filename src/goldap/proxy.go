4999082610e58ae4e18887c245f44909f5990f2e
Thomas Guillier <thomas.guillier@gmail.com>
2014-08-19 01:05:48 +0200

    Updated the LDAP Proxy: now the proxy reads each LDAP message one by one. We read the first byte to decode the tag and length, then we read exactly "length" bytes of the LDAP message SEQUENCE

--- src/goldap/proxy.go
+++ src/goldap/proxy.go
@@ -68,18 +68,19 @@ func (p *Proxy) Start() {
 }
 
 func (p *Proxy) ReadClient() {
-	buf := make([]byte, 1024)
 	messageid := 1
 	for {
-		// @TODO: read the tag and length, then get the right amount of data
-		n, err := p.clientConn.Read(buf)
-		if err != nil || n == 0 {
+		var err error
+		var bytes *[]byte
+		bytes, err = p.readLdapMessageBytes(p.clientConn)
+		if err != nil {
 			p.clientConn.Close()
 			log.Printf("%s: %s", p.name, "CLIENT DISCONNECTED")
 			break
 		}
+
 		messageid++
-		message := Message{id: messageid, source: "CLIENT", bytes: buf[0:n]}
+		message := Message{id: messageid, source: "CLIENT", bytes: *bytes}
 		p.dumpChan <- message
 		p.serverChan <- message
 	}
@@ -92,19 +93,18 @@ func (p *Proxy) WriteServer() {
 }
 
 func (p *Proxy) ReadServer() {
-	buf := make([]byte, 1024*1024)
 	messageid := 0
 	for {
-		// @TODO: read the tag and length, then get the right amount of data
-		n, err := p.serverConn.Read(buf)
-		if err != nil || n == 0 {
+		var err error
+		var bytes *[]byte
+		bytes, err = p.readLdapMessageBytes(p.serverConn)
+		if err != nil {
 			p.serverConn.Close()
 			log.Printf("%s: %s", p.name, "SERVER DISCONNECTED")
 			break
 		}
-
 		messageid++
-		message := Message{id: messageid, source: "SERVER", bytes: buf[0:n]}
+		message := Message{id: messageid, source: "SERVER", bytes: *bytes}
 		p.dumpChan <- message
 		p.clientChan <- message
 	}
@@ -144,5 +144,105 @@ func (p *Proxy) DecodeMessage(bytes []byte) (ret LDAPMessage, err error) {
 		}
 	}()
 	ret, err = ReadLDAPMessage(&Bytes{offset: 0, bytes: bytes})
+	return
+}
+
+
+func (p *Proxy) readLdapMessageBytes(conn net.Conn) (ret *[]byte, err error){
+	var bytes []byte
+	var tagAndLength tagAndLength
+	tagAndLength, err = p.readTagAndLength(conn, &bytes)
+	if err != nil {
+		return
+	}
+	p.readBytes(conn, &bytes, tagAndLength.length)
+	return &bytes, err
+}
+
+
+// Read "length" bytes from the connection
+// Append the read bytes to "bytes"
+// Return the last read byte
+func (p *Proxy) readBytes(conn net.Conn, bytes *[]byte, length int) (b byte, err error){
+	newbytes := make([]byte, length)
+	n, err := conn.Read(newbytes)
+	if n != length {
+		fmt.Errorf("%d bytes read instead of %d", n, length)
+	} else if err != nil {
+		return
+	}
+	*bytes = append(*bytes, newbytes...)
+	b = (*bytes)[len(*bytes)-1]
+	return
+}
+
+// parseTagAndLength parses an ASN.1 tag and length pair from a live connection
+// into a byte slice. It returns the parsed data and the new offset. SET and
+// SET OF (tag 17) are mapped to SEQUENCE and SEQUENCE OF (tag 16) since we
+// don't distinguish between ordered and unordered objects in this code.
+func (p *Proxy) readTagAndLength(conn net.Conn, bytes *[]byte) (ret tagAndLength, err error) {
+	// offset = initOffset
+	//b := bytes[offset]
+	//offset++
+	var b byte
+	b, err = p.readBytes(conn, bytes, 1)
+	if err != nil {
+		return
+	}
+	ret.class = int(b >> 6)
+	ret.isCompound = b&0x20 == 0x20
+	ret.tag = int(b & 0x1f)
+
+//	// If the bottom five bits are set, then the tag number is actually base 128
+//	// encoded afterwards
+//	if ret.tag == 0x1f {
+//		ret.tag, err = parseBase128Int(conn, bytes)
+//		if err != nil {
+//			return
+//		}
+//	}
+	// We are expecting the LDAP sequence tag 0x30 as first byte
+	if b != 0x30 {
+		panic(fmt.Sprintf("Expecting 0x30 as first byte, but got %#x instead", b))
+	}
+
+
+	b, err = p.readBytes(conn, bytes, 1)
+	if err != nil {
+		return
+	}
+	if b&0x80 == 0 {
+		// The length is encoded in the bottom 7 bits.
+		ret.length = int(b & 0x7f)
+	} else {
+		// Bottom 7 bits give the number of length bytes to follow.
+		numBytes := int(b & 0x7f)
+		if numBytes == 0 {
+			err = SyntaxError{"indefinite length found (not DER)"}
+			return
+		}
+		ret.length = 0
+		for i := 0; i < numBytes; i++ {
+
+			b, err = p.readBytes(conn, bytes, 1)
+			if err != nil {
+				return
+			}
+			if ret.length >= 1<<23 {
+				// We can't shift ret.length up without
+				// overflowing.
+				err = StructuralError{"length too large"}
+				return
+			}
+			ret.length <<= 8
+			ret.length |= int(b)
+			if ret.length == 0 {
+				// DER requires that lengths be minimal.
+				err = StructuralError{"superfluous leading zeros in length"}
+				return
+			}
+		}
+	}
+
 	return
 }
